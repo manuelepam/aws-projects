1c66f2802e44825807685fab5351a9423350c59e
manuelepam <manuel.epam.etame@gmail.com>
2026-08-27 15:15:28 +0100

    Add Kinesis Lambda infrastructure

--- cybersecurity-threat-detection/infra/main.tf
+++ cybersecurity-threat-detection/infra/main.tf
@@ -44,3 +44,144 @@ resource "aws_kinesis_stream" "security_events" {
 
   tags = local.common_tags
 }
+
+data "archive_file" "lambda_package" {
+  type        = "zip"
+  output_path = "${path.module}/.terraform/lambda_package.zip"
+
+  source {
+    content  = file("${path.module}/../src/__init__.py")
+    filename = "src/__init__.py"
+  }
+
+  source {
+    content  = file("${path.module}/../src/detector.py")
+    filename = "src/detector.py"
+  }
+
+  source {
+    content  = file("${path.module}/../src/lambda_handler.py")
+    filename = "src/lambda_handler.py"
+  }
+}
+
+resource "aws_iam_role" "lambda_execution" {
+  name = "${local.project_name}-${var.environment}-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = local.common_tags
+}
+
+resource "aws_cloudwatch_log_group" "lambda_logs" {
+  name              = "/aws/lambda/${local.project_name}-${var.environment}"
+  retention_in_days = 14
+
+  tags = local.common_tags
+}
+
+resource "aws_iam_role_policy" "lambda_permissions" {
+  name = "${local.project_name}-${var.environment}-lambda-permissions"
+  role = aws_iam_role.lambda_execution.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Sid    = "ReadSecurityEvents"
+        Effect = "Allow"
+
+        Action = [
+          "kinesis:DescribeStream",
+          "kinesis:DescribeStreamSummary",
+          "kinesis:GetRecords",
+          "kinesis:GetShardIterator",
+          "kinesis:ListShards"
+        ]
+
+        Resource = aws_kinesis_stream.security_events.arn
+      },
+      {
+        Sid      = "ListKinesisStreams"
+        Effect   = "Allow"
+        Action   = "kinesis:ListStreams"
+        Resource = "*"
+      },
+      {
+        Sid    = "DecryptKinesisRecords"
+        Effect = "Allow"
+        Action = "kms:Decrypt"
+
+        Resource = "*"
+
+        Condition = {
+          StringEquals = {
+            "kms:ViaService" = "kinesis.${var.aws_region}.amazonaws.com"
+          }
+        }
+      },
+      {
+        Sid    = "WriteLambdaLogs"
+        Effect = "Allow"
+
+        Action = [
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+
+        Resource = "${aws_cloudwatch_log_group.lambda_logs.arn}:*"
+      }
+    ]
+  })
+}
+
+resource "aws_lambda_function" "threat_detector" {
+  function_name = "${local.project_name}-${var.environment}"
+  description   = "Detects suspicious authentication activity from Kinesis events"
+
+  filename         = data.archive_file.lambda_package.output_path
+  source_code_hash = data.archive_file.lambda_package.output_base64sha256
+
+  role    = aws_iam_role.lambda_execution.arn
+  runtime = "python3.13"
+  handler = "src.lambda_handler.lambda_handler"
+
+  memory_size = 128
+  timeout     = 10
+
+  tags = local.common_tags
+
+  depends_on = [
+    aws_iam_role_policy.lambda_permissions,
+    aws_cloudwatch_log_group.lambda_logs
+  ]
+}
+
+resource "aws_lambda_event_source_mapping" "kinesis_to_lambda" {
+  event_source_arn  = aws_kinesis_stream.security_events.arn
+  function_name     = aws_lambda_function.threat_detector.arn
+  starting_position = "LATEST"
+
+  maximum_retry_attempts        = 3
+  maximum_record_age_in_seconds = 3600
+
+  batch_size = 100
+  enabled    = true
+
+  function_response_types = ["ReportBatchItemFailures"]
+}
--- cybersecurity-threat-detection/infra/outputs.tf
+++ cybersecurity-threat-detection/infra/outputs.tf
@@ -7,3 +7,23 @@ output "kinesis_stream_arn" {
   description = "ARN of the Kinesis security-events stream"
   value       = aws_kinesis_stream.security_events.arn
 }
+
+output "lambda_function_name" {
+  description = "Name of the threat-detection Lambda function"
+  value       = aws_lambda_function.threat_detector.function_name
+}
+
+output "lambda_function_arn" {
+  description = "ARN of the threat-detection Lambda function"
+  value       = aws_lambda_function.threat_detector.arn
+}
+
+output "lambda_log_group_name" {
+  description = "CloudWatch log group used by the Lambda function"
+  value       = aws_cloudwatch_log_group.lambda_logs.name
+}
+
+output "event_source_mapping_uuid" {
+  description = "Identifier of the Kinesis-to-Lambda event-source mapping"
+  value       = aws_lambda_event_source_mapping.kinesis_to_lambda.uuid
+}
